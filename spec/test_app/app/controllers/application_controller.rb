4bf02e1aa8b1b1bcee7014a22d4e46ba125433f9
Avalon Van der Horst <avalon@avalonvorstsmac.home>
2026-03-27 09:34:31 +0100

    Improve test_app for development purposes

--- spec/test_app/app/controllers/application_controller.rb
+++ spec/test_app/app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+
 def current_user
   User.first!
 end
 
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  # Changes to the importmap will invalidate the etag for HTML responses
+  stale_when_importmap_changes
 end
